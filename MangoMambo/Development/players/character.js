a1a64d753a37a2a30b6d0ead352823ab88faf7db
Mario <55460661+Kratser@users.noreply.github.com>
2019-10-18 15:57:18 +0200

    Character update

--- MangoMambo/Development/players/character.js
+++ MangoMambo/Development/players/character.js
@@ -3,36 +3,79 @@
 // Lémur: Collider: {25, 42}, VelMax: , Acel: , Alt Salto: , Vel Caída:
 // Tucán: Collider: {25, 20}, VelMax: , Acel: , Alt Salto: , Vel Caída:
 class Character extends Phaser.GameObjects.Sprite{
-    constructor (scene, type, x, y, cursors, colliderSize = [,], maxVelocity = 300, acceleration = 480, jumpHeight = 400, fallSpeed = 100) {
+    constructor (scene, id, type, physics, x, y, colliderSize = [,], maxVelocity = 300, acceleration = 480, jumpHeight = 400, fallSpeed = 100) {
         // Llamada al padre del objeto
         super(scene, x, y, type);
 
         //Atributos del personaje
         this.scene = scene
+        this.id = id;
         this.type = type; //Sprite
-        this.cursors = cursors;// 0 arriba, 1 izquierda, 2 abajo, 3 derecha, movimiento
+        this.physics = physics;
+        this.x = x;
+        this.y = y;
         this.colliderSize = colliderSize;
         this.maxVelocity = maxVelocity;
         this.acceleration = acceleration;
         this.jumpHeight = jumpHeight;
         this.fallSpeed = fallSpeed;
+
+        this.cursors;// 0 arriba, 1 izquierda, 2 abajo, 3 derecha, movimiento
         // Se añade a la escena al hacer el new
         scene.add.existing(this);
 
         // Se activan las físicas de la escena y las colisiones con los bordes del lienzo
-        scene.physics.world.enable(this);
-        this.body.setCollideWorldBounds(true);
-
+        if(this.physics){
+            scene.physics.world.enable(this);
+            this.body.setCollideWorldBounds(true);
+        
         //Propiedades del personaje como el rozamiento o el escalado (revisar)
         this.body.maxVelocity.x = this.maxVelocity;
         this.body.setAllowDrag();
         this.body.setDragX(480);
         this.body.setSize(this.colliderSize[0], this.colliderSize[1]); // Para cambiar el collider
-
+        }
     }
 
     preload(){
-        
+        this.cursors1 = []; // Esto vendrá dado en la clase Player
+        this.cursors1[0] = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        this.cursors1[1] = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        this.cursors1[2] = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        this.cursors1[3] = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+
+        this.cursors2 = []; // Esto vendrá dado en la clase Player
+        this.cursors2[0] = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.I);
+        this.cursors2[1] = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.J);
+        this.cursors2[2] = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.K);
+        this.cursors2[3] = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.L);
+
+        this.cursors3 = []; // Esto vendrá dado en la clase Player
+        this.cursors3[0] = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+        this.cursors3[1] = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        this.cursors3[2] = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+        this.cursors3[3] = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+
+        this.cursors4 = []; // Esto vendrá dado en la clase Player
+        this.cursors4[0] = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.NUMPAD_8);
+        this.cursors4[1] = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.NUMPAD_4);
+        this.cursors4[2] = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.NUMPAD_5);
+        this.cursors4[3] = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.NUMPAD_6);
+
+        switch (this.id){
+            case 1:
+                this.cursors = this.cursors1;
+                break;
+            case 2:
+                this.cursors = this.cursors2;
+                break;
+            case 3:
+                this.cursors = this.cursors3;
+                break;
+            case 4:
+                this.cursors = this.cursors4;
+                break;
+        }
     }
 
     create() {
