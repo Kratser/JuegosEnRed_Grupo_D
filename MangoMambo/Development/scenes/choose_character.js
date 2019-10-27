df49c008e88cf3ab3b32a8e44e4290acbc86f363
Mireya Funke <36482605+mfpHeras@users.noreply.github.com>
2019-10-27 13:35:06 +0100

    New version od the choose_character scene.

--- MangoMambo/Development/scenes/choose_character.js
+++ MangoMambo/Development/scenes/choose_character.js
@@ -9,6 +9,10 @@ class ChooseCharacter extends Phaser.Scene {
         this.characters;
         this.charactersSelected;
 
+        // Arrays para el texto
+        this.names;
+        this.habilities;
+
         this.cursors1;
         this.cursors2;
         this.cursors3;
@@ -24,12 +28,47 @@ class ChooseCharacter extends Phaser.Scene {
 
     preload() {
         // Se cargan las imágenes
-        this.load.image("character_background", "../Design/Stages/Backgrounds/choose_player.png");
+        this.load.image("character_background", "../Design/Stages/Backgrounds/choose_character_background.png");
 
-        this.load.image("palm", "../Design/Characters/Palm/palm_idle_00.png");
-        this.load.image("dino", "../Design/Characters/Dino/dino_idle_00.png");
-        this.load.image("toucan", "../Design/Characters/Toucan/toucan_idle_00.png");
-        this.load.image("lemur", "../Design/Characters/Lemur/lemur_idle_00.png");
+        // Imagenes de los personajes
+        this.load.image("palm", "../Design/Characters/Palm/palm_choose.png");
+        this.load.image("dino", "../Design/Characters/Dino/dino_choose.png");
+        this.load.image("toucan", "../Design/Characters/Toucan/toufat_choose.png");
+        this.load.image("lemur", "../Design/Characters/Lemur/lemur_choose.png");
+
+        // Nombre de los personajes
+        // Verde
+        this.load.image("palm_G_name", "../Design/Objects/Text/palm_G_name.png");
+        this.load.image("dino_G_name", "../Design/Objects/Text/dino_G_name.png");
+        this.load.image("toufat_G_name", "../Design/Objects/Text/toufat_G_name.png");
+        this.load.image("lemur_G_name", "../Design/Objects/Text/lemur_G_name.png");
+        // Rosa
+        this.load.image("palm_P_name", "../Design/Objects/Text/palm_P_name.png");
+        this.load.image("dino_P_name", "../Design/Objects/Text/dino_P_name.png");
+        this.load.image("toufat_P_name", "../Design/Objects/Text/toufat_P_name.png");
+        this.load.image("lemur_P_name", "../Design/Objects/Text/lemur_P_name.png");
+        // Azul
+        this.load.image("palm_B_name", "../Design/Objects/Text/palm_B_name.png");
+        this.load.image("dino_B_name", "../Design/Objects/Text/dino_B_name.png");
+        this.load.image("toufat_B_name", "../Design/Objects/Text/toufat_B_name.png");
+        this.load.image("lemur_B_name", "../Design/Objects/Text/lemur_B_name.png");
+        // Amarillo
+        this.load.image("palm_Y_name", "../Design/Objects/Text/palm_Y_name.png");
+        this.load.image("dino_Y_name", "../Design/Objects/Text/dino_Y_name.png");
+        this.load.image("toufat_Y_name", "../Design/Objects/Text/toufat_Y_name.png");
+        this.load.image("lemur_Y_name", "../Design/Objects/Text/lemur_Y_name.png");
+
+        // Descripción habilidades de los personajes
+        this.load.image("palm_hab", "../Design/Objects/Text/palm_hab.png");
+        this.load.image("dino_hab", "../Design/Objects/Text/dino_hab.png");
+        this.load.image("toufat_hab", "../Design/Objects/Text/toufat_hab.png");
+        this.load.image("lemur_hab", "../Design/Objects/Text/lemur_hab.png");
+
+        this.load.image("ready", "../Design/Objects/Text/ready.png");
+
+        // Arrays para los textos
+        this.names = [];
+        this.habilities = [];
 
         this.players = [ {active:false, selected: false}, {active:false, selected: false}, 
                          {active:false, selected: false}, {active:false, selected: false}]; // Jugadores
@@ -76,6 +115,52 @@ class ChooseCharacter extends Phaser.Scene {
 
     create() {
         this.add.image(0, 0, "character_background").setOrigin(0,0); // Fondo
+
+        // Texto ready (seleccionado)
+        this.ready1 = this.add.image(171.50, 239.05, "ready").setDepth(1);
+        this.ready2 = this.add.image(457.50, 239.05, "ready").setDepth(1);
+        this.ready3 = this.add.image(743.50, 239.05, "ready").setDepth(1);
+        this.ready4 = this.add.image(1034.50, 239.05, "ready").setDepth(1);
+
+        this.ready1.alpha = 0;
+        this.ready2.alpha = 0;
+        this.ready3.alpha = 0;
+        this.ready4.alpha = 0;
+
+        // Texto habilidades
+        this.player1_hab = this.add.image(167.28, 389.96, "palm_hab");
+        this.player2_hab = this.add.image(455, 389.96, "palm_hab");
+        this.player3_hab = this.add.image(744.28, 389.96, "palm_hab");
+        this.player4_hab = this.add.image(1035.28, 389.96, "palm_hab");
+
+        // Para que no aparezcan de primeras
+        this.player1_hab.alpha = 0;
+        this.player2_hab.alpha = 0;
+        this.player3_hab.alpha = 0;
+        this.player4_hab.alpha = 0;
+
+        this.habilities = [{hab: this.player1_hab, img: "palm_hab"}, 
+        {hab: this.player2_hab, img: "dino_hab"}, 
+        {hab: this.player3_hab, img: "toufat_hab"}, 
+        {hab: this.player4_hab, img: "lemur_hab"}];
+
+        // Texto nombres
+        this.player1_name = this.add.image(167.28, 63, "palm_G_name");
+        this.player2_name = this.add.image(459.28, 63, "palm_P_name");
+        this.player3_name = this.add.image(744.28, 63, "palm_B_name");
+        this.player4_name = this.add.image(1035.28, 63, "palm_Y_name");
+
+        // Para que no aparezcan de primeras
+        this.player1_name.alpha = 0;
+        this.player2_name.alpha = 0;
+        this.player3_name.alpha = 0;
+        this.player4_name.alpha = 0;
+
+        this.names = [{name: this.player1_name, img: ["palm_G_name","dino_G_name","toufat_G_name","lemur_G_name"]}, 
+        {name: this.player2_name, img: ["palm_P_name","dino_P_name","toufat_P_name","lemur_P_name"]}, 
+        {name: this.player3_name, img: ["palm_B_name","dino_B_name","toufat_B_name","lemur_B_name"]}, 
+        {name: this.player4_name, img: ["palm_Y_name","dino_Y_name","toufat_Y_name","lemur_Y_name"]}];
+
     }//Fin create
 
     update() {
@@ -83,9 +168,10 @@ class ChooseCharacter extends Phaser.Scene {
         if (!this.players[0].active){// Si el jugador 1 no se encuentra activo
             // Si se pulsa la tecla WASD, se añade el jugador 1 a la lista de jugadores
             if (Phaser.Input.Keyboard.JustDown(this.cursors1[0]) || Phaser.Input.Keyboard.JustDown(this.cursors1[1]) || Phaser.Input.Keyboard.JustDown(this.cursors1[2]) || Phaser.Input.Keyboard.JustDown(this.cursors1[3])) {
-                this.characters[0] = new Character(this, 1, "palm", false, 200, 270);
+                this.characters[0] = new Character(this, 1, "palm", false, 163, 224.5);
                 this.players[0].active = true;
-                this.numPlayers++;  
+                this.numPlayers++;
+                this.ChangeText(this.selector1, 0);// Que aparezca la habilidad/nombre al empezar a seleccionar
             }
         }else {// Si el jugador 1 ya se encuentra activo
             // A para cambiar de personaje
@@ -117,11 +203,13 @@ class ChooseCharacter extends Phaser.Scene {
             else if (Phaser.Input.Keyboard.JustDown(this.cursors1[2]) && !this.charactersSelected[this.selector1]){
                 this.charactersSelected[this.selector1] = true;
                 this.players[0].selected = true;
+                this.ready1.alpha = 1;// Personaje seleccionado, preparado para jugar
             }
             // W para deseleccionar personaje (si ya ha seleccionado alguno)
             else if (Phaser.Input.Keyboard.JustUp(this.cursors1[0]) && this.players[0].selected){
                 this.charactersSelected[this.selector1] = false;
                 this.players[0].selected = false;
+                this.ready1.alpha = 0;
             }
             // W para salir de la partida
             if (Phaser.Input.Keyboard.JustDown(this.cursors1[0]) && !this.players[0].selected){
@@ -129,16 +217,19 @@ class ChooseCharacter extends Phaser.Scene {
                 this.characters[0].destroy();
                 this.players[0].active = false;
                 this.selector1 = 0;
+                this.habilities[0].hab.alpha = 0;// Ocultar habilidad
+                this.names[0].name.alpha = 0;// Ocultar nombre
             }
-        }//Fin jugador 1
+        }// Fin jugador 1
 
         // Creación y cambio del personaje del jugador 2
         if (!this.players[1].active){// Si el jugador 2 no se encuentra activo
             // Si se pulsa IJKL, se añade el jugador 2 a la lista de jugadores
             if (Phaser.Input.Keyboard.JustDown(this.cursors2[0]) || Phaser.Input.Keyboard.JustDown(this.cursors2[1]) || Phaser.Input.Keyboard.JustDown(this.cursors2[2]) || Phaser.Input.Keyboard.JustDown(this.cursors2[3])) {
-                this.characters[1] = new Character(this, 2, "palm", false, 460, 270);
+                this.characters[1] = new Character(this, 2, "palm", false, 453, 224.5);
                 this.players[1].active = true;
                 this.numPlayers++;
+                this.ChangeText(this.selector2, 1);// Que aparezca la habilidad/nombre al empezar a seleccionar
             }
         }else {
             // J para cambiar de personaje
@@ -170,11 +261,13 @@ class ChooseCharacter extends Phaser.Scene {
             else if (Phaser.Input.Keyboard.JustDown(this.cursors2[2]) && !this.charactersSelected[this.selector2]){
                 this.charactersSelected[this.selector2] = true;
                 this.players[1].selected = true;
+                this.ready2.alpha = 1;// Personaje seleccionado, preparado para jugar
             }
             // I para deseleccionar personaje (Si no tiene uno seleccionado)
             else if (Phaser.Input.Keyboard.JustUp(this.cursors2[0]) && this.players[1].selected){
                 this.charactersSelected[this.selector2] = false;
                 this.players[1].selected = false;
+                this.ready2.alpha = 0;
             }
             // I para salir de la partida
             if (Phaser.Input.Keyboard.JustDown(this.cursors2[0]) && !this.players[1].selected){
@@ -182,6 +275,8 @@ class ChooseCharacter extends Phaser.Scene {
                 this.characters[1].destroy();
                 this.players[1].active = false;
                 this.selector2 = 0;
+                this.habilities[1].hab.alpha = 0; // Ocultar habilidad
+                this.names[1].name.alpha = 0; // Ocultar nombre
             }
         }//Fin jugador 2
         
@@ -189,9 +284,10 @@ class ChooseCharacter extends Phaser.Scene {
         if (!this.players[2].active){ // Si el jugador 3 no se encuentra activo
             // Si se pulsa alguna flecha de dirección se añade el jugador 3 a la lista de personajes
             if (Phaser.Input.Keyboard.JustDown(this.cursors3[0]) || Phaser.Input.Keyboard.JustDown(this.cursors3[1]) || Phaser.Input.Keyboard.JustDown(this.cursors3[2]) || Phaser.Input.Keyboard.JustDown(this.cursors3[3])) {
-                this.characters[2] = new Character(this, 3, "palm", false, 735, 270);
+                this.characters[2] = new Character(this, 3, "palm", false, 740.50, 224.5);
                 this.players[2].active = true;
                 this.numPlayers++;
+                this.ChangeText(this.selector3, 2);// Que aparezca la habilidad/nombre al empezar a seleccionar
             }
         }else {
             // LEFT para cambiar de personaje
@@ -223,11 +319,13 @@ class ChooseCharacter extends Phaser.Scene {
             else if (Phaser.Input.Keyboard.JustDown(this.cursors3[2]) && !this.charactersSelected[this.selector3]){
                 this.charactersSelected[this.selector3] = true;
                 this.players[2].selected = true;
+                this.ready3.alpha = 1;// Personaje seleccionado, preparado para jugar
             }
             // UP para deseleccionar personaje (si no tiene uno seleccionado) 
             else if (Phaser.Input.Keyboard.JustUp(this.cursors3[0]) && this.players[2].selected){
                 this.charactersSelected[this.selector3] = false;
                 this.players[2].selected = false;
+                this.ready3.alpha = 0;
             }
             // UP para salir de la partida
             if (Phaser.Input.Keyboard.JustDown(this.cursors3[0]) && !this.players[2].selected){
@@ -235,6 +333,8 @@ class ChooseCharacter extends Phaser.Scene {
                 this.characters[2].destroy();
                 this.players[2].active = false;
                 this.selector3 = 0;
+                this.habilities[2].hab.alpha = 0;// Ocultar habilidad
+                this.names[2].name.alpha = 0;// Ocultar nombre
             }
         }//Fin jugador 3
         
@@ -242,9 +342,10 @@ class ChooseCharacter extends Phaser.Scene {
         if (!this.players[3].active){ // Si el jugador 4 no se encuentra activo
             // 8456 en el NUMPAD para añadir al jugador 4 a la lista de personajes
             if (Phaser.Input.Keyboard.JustDown(this.cursors4[0]) || Phaser.Input.Keyboard.JustDown(this.cursors4[1]) || Phaser.Input.Keyboard.JustDown(this.cursors4[2]) || Phaser.Input.Keyboard.JustDown(this.cursors4[3])) {
-                this.characters[3] = new Character(this, 4, "palm", false, 990, 270);
+                this.characters[3] = new Character(this, 4, "palm", false, 1022, 224.5);
                 this.players[3].active = true;
                 this.numPlayers++;
+                this.ChangeText(this.selector4, 3);// Que aparezca la habilidad/nombre al empezar a seleccionar
             }
         }else {
             // NUMPAD_4 para cambiar de personaje
@@ -276,11 +377,13 @@ class ChooseCharacter extends Phaser.Scene {
             else if (Phaser.Input.Keyboard.JustDown(this.cursors4[2]) && !this.charactersSelected[this.selector4]){
                 this.charactersSelected[this.selector4] = true;
                 this.players[3].selected = true;
+                this.ready4.alpha = 1;// Personaje seleccionado, preparado para jugar
             }
             // NUMPAD_8 para deseleccionar personaje (Si ya tiene uno seleccionado)
             else if (Phaser.Input.Keyboard.JustUp(this.cursors4[0]) && this.players[3].selected){
                 this.charactersSelected[this.selector4] = false;
                 this.players[3].selected = false;
+                this.ready4.alpha = 0;
             }
             // NUMPAD_8 para salir de la partida
             if (Phaser.Input.Keyboard.JustDown(this.cursors4[0]) && !this.players[3].selected){
@@ -288,6 +391,8 @@ class ChooseCharacter extends Phaser.Scene {
                 this.characters[3].destroy();
                 this.players[3].active = false;
                 this.selector4 = 0;
+                this.habilities[3].hab.alpha = 0;// Ocultar habilidad
+                this.names[3].name.alpha = 0;// Ocultar nombre
             }
         }//Fin jugador 4
 
@@ -296,9 +401,9 @@ class ChooseCharacter extends Phaser.Scene {
             this.scene.start("level_1");
         }
 
-    }//Fin Update
+    }// Fin Update
 
-    changeCharacter(charactersArray, characterid, selector){
+    changeCharacter(charactersArray, characterid, selector, names){
         switch(selector){
             case 0:
                 this.characterAux = new Character(this, characterid+1, "palm", false, charactersArray[characterid].x, charactersArray[characterid].y);
@@ -311,15 +416,28 @@ class ChooseCharacter extends Phaser.Scene {
                     this.characters[characterid] = this.characterAux;
                 break;
             case 2:
-                    this.characterAux = new Character(this, characterid+1, "lemur", false, charactersArray[characterid].x, charactersArray[characterid].y);
-                    this.characters[characterid].destroy();
-                    this.characters[characterid] = this.characterAux;
-                break;
-            case 3:
                     this.characterAux = new Character(this, characterid+1, "toucan", false, charactersArray[characterid].x, charactersArray[characterid].y);
                     this.characters[characterid].destroy();
                     this.characters[characterid] = this.characterAux;
                 break;
+            case 3:
+                    this.characterAux = new Character(this, characterid+1, "lemur", false, charactersArray[characterid].x, charactersArray[characterid].y);
+                    this.characters[characterid].destroy();
+                    this.characters[characterid] = this.characterAux;
+                break;
         }
-    }//Fin changeCharacter
+
+        // Cambia el texto explicativo de la habilidad y el nombre en función del personaje y el jugador
+        this.ChangeText(selector, characterid);
+    }// Fin changeCharacter
+
+    ChangeText(selector, characterid){
+        var habilityAux = this.add.image(this.habilities[characterid].hab.x, this.habilities[characterid].hab.y, this.habilities[selector].img);
+        this.habilities[characterid].hab.destroy();
+        this.habilities[characterid].hab = habilityAux;
+
+        var nameAux = this.add.image(this.names[characterid].name.x, this.names[characterid].name.y, this.names[characterid].img[selector]);
+        this.names[characterid].name.destroy();
+        this.names[characterid].name = nameAux;
+    }// Fin ChangeHabilities
 }
