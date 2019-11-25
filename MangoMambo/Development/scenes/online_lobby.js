5f62a01d38dbe90e31ba92049ccf486bea30c118
Kratser <mario.belen99@gmail.com>
2019-11-25 19:49:18 +0100

    My player animation added on online lobby scene

--- MangoMambo/Development/scenes/online_lobby.js
+++ MangoMambo/Development/scenes/online_lobby.js
@@ -46,18 +46,11 @@ class OnlineLobby extends Phaser.Scene{
         // Se carga la imagen de fondo
         this.load.image("lobby_background", "./Design/Stages/Backgrounds/lobby_background.png");
         // Jugadores
-        /*this.load.image("player1", "./Design/player1.png");
-        this.load.image("player2", "./Design/player2.png");
-        this.load.image("player3", "./Design/player3.png");
-        this.load.image("player4", "./Design/player4.png");*/
-        this.load.image("palm_G_name", "./Design/Objects/Text/palm_G_name.png");
-        this.load.image("dino_P_name", "./Design/Objects/Text/dino_P_name.png");
-        this.load.image("toufat_B_name", "./Design/Objects/Text/toufat_B_name.png");
-        this.load.image("lemur_Y_name", "./Design/Objects/Text/lemur_Y_name.png");
-        this.player1;
-        this.player2;
-        this.player3;
-        this.player4;
+        this.load.image("player1_online", "./Design/Objects/Text/player1_online.png");
+        this.load.image("player2_online", "./Design/Objects/Text/player2_online.png");
+        this.load.image("player3_online", "./Design/Objects/Text/player3_online.png");
+        this.load.image("player4_online", "./Design/Objects/Text/player4_online.png");
+        this.playersImg;
         // Teclas
         this.cursors;
         // Estado del servidor
@@ -69,14 +62,15 @@ class OnlineLobby extends Phaser.Scene{
         // Se crea la imagen de fondo
         this.add.image(0, 0, "lobby_background").setOrigin(0, 0);
         // Jugadores
-        this.player1 = this.add.image(0, 0, "palm_G_name");
-        this.player1.setAlpha(0);
-        this.player2 = this.add.image(0, 20, "dino_P_name");
-        this.player2.setAlpha(0);
-        this.player3 = this.add.image(0, 40, "toufat_B_name");
-        this.player3.setAlpha(0);
-        this.player4 = this.add.image(0, 60, "lemur_Y_name");
-        this.player4.setAlpha(0);
+        this.playersImg = [];
+        this.playersImg[0] = this.add.image(273.20, 138.95, "player1_online");
+        this.playersImg[0].setAlpha(0);
+        this.playersImg[1] = this.add.image(273.20, 238.50, "player2_online");
+        this.playersImg[1].setAlpha(0);
+        this.playersImg[2] = this.add.image(273.20, 341.19, "player3_online");
+        this.playersImg[2].setAlpha(0);
+        this.playersImg[3] = this.add.image(273.20, 442.83, "player4_online");
+        this.playersImg[3].setAlpha(0);
         // Teclas
         this.cursors = [];
         this.cursors[0] = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
@@ -85,6 +79,15 @@ class OnlineLobby extends Phaser.Scene{
         this.cursors[3] = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
         // Se añade el cliente a la lista de jugadores
         this.players[this.myPlayer.id] = this.myPlayer;
+        // Tween de la imagen del jugador
+        var tweenPlayer = this.tweens.add({
+            targets: this.playersImg[this.myPlayer.id],
+            alpha: { from: 0.35, to: 1 },
+            duration: 1000,
+            ease: 'Sine.easeInOut',
+            yoyo: true,
+            repeat: -1
+        });
         // Temporizador para comprobar el estado de los jugadores
         this.time.addEvent({ delay: 5000, callback: this.checkPlayers, callbackScope: this, repeat: -1});
         // Imagen del estado del servidor
@@ -96,27 +99,6 @@ class OnlineLobby extends Phaser.Scene{
         // Si el servidor está activo
         if (this.serverStatus){
             this.serverStatusImg.setAlpha(0);
-            // Mostrar a los jugadores si entran en el lobby
-            if (this.players[0] && this.players[0].isConnected) {
-                this.player1.setAlpha(1);
-            } else {
-                this.player1.setAlpha(0);
-            }
-            if (this.players[1] && this.players[1].isConnected) {
-                this.player2.setAlpha(1);
-            } else {
-                this.player2.setAlpha(0);
-            }
-            if (this.players[2] && this.players[2].isConnected) {
-                this.player3.setAlpha(1);
-            } else {
-                this.player3.setAlpha(0);
-            }
-            if (this.players[3] && this.players[3].isConnected) {
-                this.player4.setAlpha(1);
-            } else {
-                this.player4.setAlpha(0);
-            }
             // Si se pulsa la tecla ESC
             if (Phaser.Input.Keyboard.JustDown(this.cursors[1])) {
                 var that = this;
@@ -149,6 +131,7 @@ class OnlineLobby extends Phaser.Scene{
             method: "GET",
             url: "http://"+ this.ip +"/mango-mambo"
         });
+        // Si establece conexión con el servidor se actualizan los jugadores
         checkStatus.done(function(data){
             that.players = data;
             console.log(that.players);
@@ -165,7 +148,37 @@ class OnlineLobby extends Phaser.Scene{
             }).done(function(data){
                 that.players[data.id] = data;
             });
+         // Mostrar a los jugadores si entran en el lobby
+            if (that.players[0] && that.players[0].isConnected && that.players[0].isReady) {
+            	that.playersImg[0].setAlpha(1);
+            } else if(that.players[0] && that.players[0].isConnected && !that.players[0].isReady){
+            	that.playersImg[0].setAlpha(0.6);
+            } else {
+            	that.playersImg[0].setAlpha(0);
+            }
+            if (that.players[1] && that.players[1].isConnected && that.players[1].isReady) {
+            	that.playersImg[1].setAlpha(1);
+            } else if (that.players[1] && that.players[1].isConnected && !that.players[1].isReady){
+            	that.playersImg[1].setAlpha(0.6);
+            } else {
+            	that.playersImg[1].setAlpha(0);
+            }
+            if (that.players[2] && that.players[2].isConnected && that.players[2].isReady) {
+            	that.playersImg[2].setAlpha(1);
+            } else if (that.players[2] && that.players[2].isConnected && !that.players[2].isReady){
+            	that.playersImg[2].setAlpha(0.6);
+            } else {
+            	that.playersImg[2].setAlpha(0);
+            }
+            if (that.players[3] && that.players[3].isConnected && that.players[3].isReady) {
+            	that.playersImg[3].setAlpha(1);
+            } else if(that.players[3] && that.players[3].isConnected && !that.players[3].isReady){
+            	that.playersImg[3].setAlpha(0.6);
+            } else {
+            	that.playersImg[3].setAlpha(0);
+            }
         });
+        // Si no se puede establecer conexión
         checkStatus.error(function(data){
             console.log("Error de conexión");
             that.serverStatus = false;
