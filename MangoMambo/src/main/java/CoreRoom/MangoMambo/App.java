9267e29f848bba2a494217fbe3d9377da99f4b14
Kratser <mario.belen99@gmail.com>
2020-01-11 19:11:54 +0100

    Choose character websocket handler added

--- MangoMambo/src/main/java/CoreRoom/MangoMambo/App.java
+++ MangoMambo/src/main/java/CoreRoom/MangoMambo/App.java
@@ -5,9 +5,27 @@ package CoreRoom.MangoMambo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
 @SpringBootApplication
-public class App 
+@EnableWebSocket
+public class App implements WebSocketConfigurer
 {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry reg){
+        reg.addHandler(chooseCharacterHandler(), "/mango-mambo/ws_choose_character")
+        .setAllowedOrigins("*");
+    }
+
+    @Bean
+    public ChooseCharacterHandler chooseCharacterHandler(){
+        return new ChooseCharacterHandler();
+    }
+
     public static void main( String[] args )
     {
     	SpringApplication.run(App.class, args);
