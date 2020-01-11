a65f3f9d15b8e6435ef856b2e08b3adf6b648aff
mireyafunkese@gmail.com <mireyafunkese@gmail.com>
2020-01-11 19:32:46 +0100

    Bug fixed.

--- MangoMambo/src/main/java/CoreRoom/MangoMambo/ChooseCharacterHandler.java
+++ MangoMambo/src/main/java/CoreRoom/MangoMambo/ChooseCharacterHandler.java
@@ -1,4 +1,4 @@
-package CoreRoom.MangoManbo;
+package CoreRoom.MangoMambo;
 
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -6,14 +6,14 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class ChooseCharacterHandler extends TextWebSocketHandler{
 
     ObjectMapper mapper = new ObjectMapper();
 
     @Override
-    protected void handleTextMessage(webSocketSession session,TextMessage message)throws Exception {
+    protected void handleTextMessage(WebSocketSession session,TextMessage message)throws Exception {
         System.out.println("Message received: " + message.getPayload());
 
       
